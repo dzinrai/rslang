f65c468b706158c8e80066ff6e970ee6c7c93d3d
dzinrai <dzinrai@mail.ru>
2020-06-18 23:23:09 +0300

    feat: add styles for auth-page and change lniks to buttons

--- src/components/auth-page/auth-page.tsx
+++ src/components/auth-page/auth-page.tsx
@@ -1,12 +1,44 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
+import { ReactComponent as AuthVector1 } from '../../img/layer-2.svg';
+import { ReactComponent as WaveVector } from '../../img/vector-1.svg';
+import './auth-page.css';
 
 function AuthPage() {
+  const history = useHistory();
+
   return (
     <div className="container">
-      <h1>RS LANG</h1>
-      <Link to="/login">Log In</Link>
-      <Link to="/signup">Sign Up</Link>
+      <div className="auth-container">
+        <AuthVector1 className="vector-auth-present" />
+
+        <button
+          className="btn btn-filled"
+          type="button"
+          onClick={() => {
+            history.push('/signup');
+          }}
+        >
+          Sign Up
+        </button>
+
+        <button
+          className="btn"
+          type="button"
+          onClick={() => {
+            history.push('/login');
+          }}
+        >
+          Log In
+        </button>
+
+      </div>
+      <span className="bg-title">
+        <span>Online</span>
+        <span>English</span>
+        <span>portal</span>
+      </span>
+      <WaveVector className="vector-auth-wave" />
     </div>
   );
 }
