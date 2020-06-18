9175f0bca9a811c9dbe373505e3fc1dcdd5a9a40
dzinrai <dzinrai@mail.ru>
2020-06-18 23:24:20 +0300

    feat: add classes and styles for signup-page

--- src/components/signup-page/signup-page.tsx
+++ src/components/signup-page/signup-page.tsx
@@ -1,5 +1,7 @@
 import React, { useRef } from 'react';
 import { useForm } from 'react-hook-form';
+import { ReactComponent as WaveVector } from '../../img/vector-1.svg';
+import './signup-page.css';
 
 interface RegisterUser {
   email: string;
@@ -19,21 +21,25 @@ function SignUpPage() {
   };
 
   return (
-    <div>
+    <div className="container">
       <form onSubmit={(e) => e.preventDefault()} autoComplete="off">
-        <span>Email</span>
+        <span className="label">Email address</span>
         <input
+          className="input"
           name="email"
+          placeholder="Email address"
           ref={register({
             required: 'Email is required',
           })}
         />
-        {errors.email && <p>{errors.email.message}</p>}
+        {errors.email && <p className="error-msg">{errors.email.message}</p>}
 
-        <span>Password</span>
+        <span className="label">Password</span>
         <input
+          className="input"
           name="password"
           type="password"
+          placeholder="Password"
           ref={register({
             required: 'You must specify a password',
             minLength: {
@@ -46,20 +52,29 @@ function SignUpPage() {
             },
           })}
         />
-        {errors.password && <p>{errors.password.message}</p>}
+        {errors.password && <p className="error-msg">{errors.password.message}</p>}
 
-        <span>Repeat password</span>
+        <span className="label">Repeat Password</span>
         <input
+          className="input"
           name="repeatPassword"
           type="password"
+          placeholder="Repeat Password"
           ref={register({
             validate: (value) => value === password.current || 'The passwords do not match',
           })}
         />
-        {errors.repeatPassword && <p>{errors.repeatPassword.message}</p>}
+        {errors.repeatPassword && <p className="error-msg">{errors.repeatPassword.message}</p>}
 
-        <button type="submit" onClick={handleSubmit(onSubmit)}>Register</button>
+        <button
+          className="btn btn-filled"
+          type="button"
+          onClick={handleSubmit(onSubmit)}
+        >
+          Sign Up
+        </button>
       </form>
+      <WaveVector className="vector-signup-wave" />
     </div>
   );
 }
