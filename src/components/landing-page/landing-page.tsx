16b8c45d164de5933fca81e6c94cb73d963404d8
Alex <alex.kryzhny@mail.ru>
2020-06-19 22:53:14 +0300

    feat: adaptive page

--- src/components/landing-page/landing-page.tsx
+++ src/components/landing-page/landing-page.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import Welcome from './welcome/welcome';
+import Adaptive from './adaptive/adaptive';
 
 function LandingPage() {
-  return <Welcome />;
+  return (
+    <>
+      <Welcome />
+      <Adaptive />
+    </>
+  );
 }
 
 export default LandingPage;
