69c1851f3fe5db790020ce2596ab8d8d80949364
Jelika <smirnova.lika.23092001@gmail.com>
2020-06-21 14:32:15 +0300

    feat: add components with styles

--- src/components/main-page/main-page.tsx
+++ src/components/main-page/main-page.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import ToDoAction from './to-do-action/to-do-action';
+import TodayProgress from './today-progress/today-progress'
+import { ReactComponent as MainPageGradientImage } from '../../img/gradient-main-page.svg';
 
 function MainPage() {
     return (
         <>
-        
         </>
     )
 }
