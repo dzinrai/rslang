98540775049c82fe9743cba6e32013d03561abf4
Jelika <smirnova.lika.23092001@gmail.com>
2020-06-22 19:55:10 +0300

    feat: add history

--- src/components/main-page/to-do-action/to-do-action.tsx
+++ src/components/main-page/to-do-action/to-do-action.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
-
 import { Button } from 'antd';
-import {Link} from 'react-router-dom'
 import 'antd/dist/antd.css';
 import styles from './to-do-action.module.css';
+import {useHistory} from 'react-router-dom';
 import { ReactComponent as ToDoActionImage } from '../../../img/layer-2-main-page.svg';
 
 function ToDoAction() {
+    const history = useHistory();
     return(
         <div className={styles.actions_container}>
             <div className={styles.btn_container}>
-       <Link to="/statistic"><Button  className={`${styles.btn_default} ${styles.btn}`} shape="round" value="large">See statistic</Button></Link>
-       <Link to="/statistic"><Button className={styles.btn} type="primary" shape="round" value="large">Play Random Game</Button></Link>
-        <Link to="/learn-words"><Button className={`${styles.btn_default} ${styles.btn}`} shape="round" value="large">Start learning words</Button></Link>
+       <Button onClick={() => {history.push('/statistic')}} className={`${styles.btn_default} ${styles.btn}`} shape="round" value="large">See statistic</Button>
+       <Button onClick={() => {history.push('/')}} className={styles.btn} type="primary" shape="round" value="large">Play Random Game</Button>
+       <Button onClick={() => {history.push('/learn-words');}} className={`${styles.btn_default} ${styles.btn}`} shape="round" value="large">Start learning words</Button>
        </div>
         <ToDoActionImage/>
         </div>
