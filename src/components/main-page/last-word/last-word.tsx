9ec366e1da0fae54a6f0ab1df0b20937c0f33600
dzinrai <dzinrai@mail.ru>
2020-06-24 18:01:40 +0300

    fix: eslint fix

--- src/components/main-page/last-word/last-word.tsx
+++ src/components/main-page/last-word/last-word.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './last-word.module.css';
-import pronunciation from '../../../img/pronunciation.svg'
+import pronunciation from '../../../img/pronunciation.svg';
 
 // import { Button } from 'antd';
 // import 'antd/dist/antd.css';
@@ -10,16 +10,19 @@ import pronunciation from '../../../img/pronunciation.svg'
 // ReactDOM.render(<DatePicker />, mountNode);
 
 function LastWord() {
-    return (
-        <div className={styles.lastWord}>
-            <p className={styles.top}>Your last word</p>
-            <div className={styles.wordContainer}>
-            <div className={styles.transcription}><img src={pronunciation} alt=""/>[snov]</div>
-                <div className={styles.word}>Snow</div>
-                <div className={styles.translation}>Снег, снегопад</div>
-            </div>
+  return (
+    <div className={styles.lastWord}>
+      <p className={styles.top}>Your last word</p>
+      <div className={styles.wordContainer}>
+        <div className={styles.transcription}>
+          <img src={pronunciation} alt="" />
+          [snov]
         </div>
-    )
+        <div className={styles.word}>Snow</div>
+        <div className={styles.translation}>Снег, снегопад</div>
+      </div>
+    </div>
+  );
 }
 
 export default LastWord;
